d96892716c1bc286eb148578933b20a40ab1b6ed
hainguyent99 <hainguyen.t99@gmail.com>
2019-08-05 09:42:45 +0700

    1

--- [Bài tập] Luyện tập mảng 01/k.js
+++ [Bài tập] Luyện tập mảng 01/k.js
@@ -1,18 +1,11 @@
 var a=[];
+var p=0;
+var b=+prompt('nhap');
+for(var i=0;i<a.length;i++){
+    a[i] = p;
+    if(a[a.length-1]<b){
 
-for(var i=0; i<10; i++){
-    a[i] = i;
+p++;
 }
-
-var V=+prompt('nhap so nguyen V');
-
-for(var i=0; i<10; i++){
-    if(V===a[i]){
-        var b=a[i];
-        a[i]=a[a.length-1];
-        a[a.length-1]=b;
-        a.pop();
-        break;  
-    } 
-    document.getElementById('demo').innerHTML = a;
 }
+document.write(a);
